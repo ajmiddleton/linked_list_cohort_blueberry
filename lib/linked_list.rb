1d11bb38d8418935480afd6b88f6b9ee78015dad
Andrew Middleton <middleton.andrew.j@gmail.com>
2014-07-10 20:30:44 -0500

    finished

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,69 @@
-class LinkedList
+require 'linked_list_item'
 
+class LinkedList
+  attr_accessor :head, :size
+
+  def initialize(*seed)
+    @head = nil
+    @size = 0
+
+    if seed.size > 0
+      seed.each {|s| self.push(s) }
+    end
+  end
+
+  def push(payload)
+    @size += 1
+    lli = LinkedListItem.new(payload)
+    if @head
+      @head.append(lli)
+    else
+      @head = lli
+    end
+
+  end
+
+  def get(index)
+    raise IndexError if index < 0 || index > @size
+    @head.fetch(index)
+  end
+
+  def last
+    if @size == 0
+      return nil
+    else
+      get(@size -1)
+    end
+  end
+
+  def to_s
+    if @size == 0
+      "| |"
+    else
+      "| " + @head.print_each + "|"
+    end
+  end
+
+  def [](index)
+    get(index)
+  end
+
+  def []=(index, new_payload)
+    @head.set(index, new_payload)
+  end
+
+  def delete(index)
+    raise IndexError if index < 0 || index > @size
+    @size -= 1
+    if index == 0
+      @head = @head.next_item
+      return
+    end
+    @head.destroy(index)
+  end
+
+  def index(payload)
+    return nil if @size == 0
+    @head.find(payload)
+  end
 end
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,98 @@
 class LinkedListItem
   include Comparable
+
+  attr_accessor :payload, :next_item
+
+  def initialize(value)
+    @payload = value
+    @next_item = nil
+  end
+
+  def <(li)
+    convert_payload_for_comparison(@payload) < convert_payload_for_comparison(li.payload)
+  end
+
+  def >(li)
+    convert_payload_for_comparison(@payload) > convert_payload_for_comparison(li.payload)
+  end
+
+  def ==(li)
+    convert_payload_for_comparison(@payload) == convert_payload_for_comparison(li.payload)
+  end
+
+  def ===(li)
+    self.object_id == li.object_id
+  end
+
+  def convert_payload_for_comparison(payload)
+    payload.to_s
+  end
+
+  def next_item=(next_obj)
+    if next_obj.object_id == self.object_id
+      raise ArgumentError
+    else
+      @next_item = next_obj
+    end
+  end
+
+  def last?
+    if @next_item
+      false
+    else
+      true
+    end
+  end
+
+  def append(lli)
+    if self.last?
+      @next_item = lli
+      return
+    else
+      @next_item.append(lli)
+    end
+  end
+
+  def fetch(index)
+    if index == 0
+      return @payload
+    end
+    @next_item.fetch(index - 1)
+  end
+
+  def set(index, new_payload)
+    if index == 0
+      @payload = new_payload
+      return
+    end
+    @next_item.set(index-1, new_payload)
+  end
+
+  def print_each(accumulator="")
+    accumulator += if last?
+      @payload.to_s + " "
+    else
+      @payload.to_s + ", "
+    end
+
+    if last?
+      return accumulator
+    end
+
+    @next_item.print_each(accumulator)
+  end
+
+  def destroy(index, prev=nil)
+    if index == 0
+      prev.next_item = @next_item
+      return
+    end
+    @next_item.destroy(index-1, self)
+  end
+
+  def find(payload, index=0)
+    return index if payload == @payload
+    return nil if last?
+    @next_item.find(payload, index+1)
+  end
 end
